ce15d7abe7d0d697db415eaad0029cc84089c30a
cmz5478 <cmz5478@psu.edu>
2025-03-16 18:47:45 -0400

    Connect to mongodb from app.js. Remove auth on the mongodb instance. Remove the mongo-express container.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node
 WORKDIR /app
 COPY package.json /app
-RUN npm install
+RUN npm install express mongoose body-parser
 COPY . /app
 CMD ["node","app.js"]
--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 
+const PORT = process.env.EXPRESS_PORT;
+const MONGO_URL = process.env.MONGO_URL;
+
+// Hello world at root endpoint
 app.get("/", function(req, res) {
     return res.send("Hello World");
 });
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000');
+// Log which port express is listening on
+app.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}`);
+});
+
+// MongoDB Connection
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
 });
