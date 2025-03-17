3b4276168e58833c7fb7d31ddeac6cd9a574c150
cmz5478 <cmz5478@psu.edu>
2025-03-16 22:04:33 -0400

    Added IsActive property to the Order model

--- models/order.js
+++ models/order.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const Order = mongoose.model('Order', new mongoose.Schema({
-    ProductID:  {type: Schema.Types.ObjectId, ref: 'Product'},
-    CustomerID: {type: Schema.Types.ObjectId, ref: 'Customer'}
+    ProductID:  { type: Schema.Types.ObjectId, ref:      'Product'  },
+    CustomerID: { type: Schema.Types.ObjectId, ref:      'Customer' },
+    IsActive:   { type: Boolean,               required: true       },
   })
 );
 
